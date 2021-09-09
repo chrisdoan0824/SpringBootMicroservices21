c762893a1b293ebb84ab7fccf705a7bfebf21376
Chris Doan <doantu@mail.uc.edu>
2021-09-09 14:41:47 -0400

    Updated / endpoint

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -3,11 +3,31 @@ package com.plantplaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+
+/**
+ * Handle the /start endpoint
+ * @author Administrator
+ *
+ */
+
 @Controller
 public class PlantPlacesController {
 	
 	@RequestMapping("/start")
 	public String start () {
+		
+		return "start";
+	}
+	
+/**
+ * Handle the / endpoint
+ * @author Administrator
+ *
+ */
+	
+	@RequestMapping("/")
+	public String index () {
 		
 		return "start";
 	}
